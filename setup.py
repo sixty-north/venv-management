357df70dd98ea9431c61e95eb69c786692c3961d
ekaterina <ekaterina@sixty-north.com>
2021-09-22 10:12:01 +0200

    Add twine to dev dependencies

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # dependencies). You can install these using the following syntax, for
     # example: $ pip install -e ".[dev,test]"
     extras_require={
-        'dev': ['black', 'bump2version'],
+        'dev': ['black', 'bump2version', 'twine'],
         # 'doc': ['sphinx', 'cartouche'],
         'test': ['hypothesis', 'pytest'],
     },
