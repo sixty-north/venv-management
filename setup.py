d1a5f97b032c0c3b94a0de300e68d4d1557a0534
ekaterina <ekaterina@sixty-north.com>
2021-09-24 11:12:26 +0200

    Bump version: 1.0.5 → 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='venv-management',
-    version='1.0.5',
+    version='1.1.0',
     packages=find_packages('source'),
 
     author='Sixty North AS',
