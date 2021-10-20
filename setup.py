eac2648e077c435c324da955633b3a02e96a29e8
Robert Smallshire <rob@sixty-north.com>
2021-10-20 17:20:34 +0200

    Bump version: 1.1.2 → 1.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='venv-management',
-    version='1.1.2',
+    version='1.1.3',
     packages=find_packages('source'),
 
     author='Sixty North AS',
