3b5ac58f9dc17a8f0adf6f9f2f3466c2b5a7c347
Robert Smallshire <rob@sixty-north.com>
2021-10-22 13:34:46 +0200

    Adds test_list_virtual_envs_with_one_env

--- tests/test_list_virtual_envs.py
+++ tests/test_list_virtual_envs.py
@@ -1,6 +1,19 @@
-from venv_management import list_virtual_envs
+import uuid
+
+from venv_management import list_virtual_envs, make_virtual_env, discard_virtual_env
 
 
-def test_list_virtual_envs():
+def test_list_virtual_envs_with_no_envs():
     envs = list_virtual_envs()
     assert envs == []
+
+
+
+def test_list_virtual_envs_with_one_env():
+    name = "venv-management-{}".format(uuid.uuid4())
+    make_virtual_env(name)
+    try:
+        envs = list_virtual_envs()
+    finally:
+        discard_virtual_env(name)
+    assert name in envs
