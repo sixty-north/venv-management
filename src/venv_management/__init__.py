08d05bc5efb416738851e0fb4f48f707baf67d7f
Robert Smallshire <rob@sixty-north.com>
2021-10-22 15:24:11 +0200

    Specific imports in __init__.py

--- src/venv_management/__init__.py
+++ src/venv_management/__init__.py
@@ -1,3 +1,39 @@
 
-from .tools import *
 from .version import __version__, __version_info__
+from .tools import (
+    check_environment,
+    has_virtualenvwrapper,
+    list_virtual_envs,
+    make_virtual_env,
+    resolve_virtual_env,
+    virtual_envs_dirpath,
+    virtual_env,
+    ensure_virtual_env,
+    remove_virtual_env,
+    discard_virtual_env,
+    python_executable_path,
+    python_name,
+    python_version,
+    lsvirtualenv,
+    mkvirtualenv,
+    rmvirtualenv,
+)
+
+__all__ = [
+    "check_environment",
+    "has_virtualenvwrapper",
+    "list_virtual_envs",
+    "make_virtual_env",
+    "resolve_virtual_env",
+    "virtual_envs_dirpath",
+    "virtual_env",
+    "ensure_virtual_env",
+    "remove_virtual_env",
+    "discard_virtual_env",
+    "python_executable_path",
+    "python_name",
+    "python_version",
+    "lsvirtualenv",
+    "mkvirtualenv",
+    "rmvirtualenv",
+]
