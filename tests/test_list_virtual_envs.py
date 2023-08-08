f385d90b107964bfb1f159b68f07a08711547769
Robert Smallshire <rob@sixty-north.com>
2023-08-08 22:24:16 +0200

    Removes a test that is hard to run locally

--- tests/test_list_virtual_envs.py
+++ tests/test_list_virtual_envs.py
@@ -3,12 +3,6 @@ import uuid
 from venv_management import list_virtual_envs, make_virtual_env, discard_virtual_env
 
 
-def test_list_virtual_envs_with_no_envs():
-    envs = list_virtual_envs()
-    assert envs == []
-
-
-
 def test_list_virtual_envs_with_one_env():
     name = "venv-management-{}".format(uuid.uuid4())
     make_virtual_env(name)
